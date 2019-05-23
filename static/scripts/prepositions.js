f6a1ab011b50963f75f8b431b751ab9f8ce44228
Derek Watson <derek.watson92@gmail.com>
2019-05-23 19:33:26 +0200

    changes the organization of the key and set arrays

--- static/scripts/prepositions.js
+++ static/scripts/prepositions.js
@@ -1,10 +1,9 @@
 // Game Dataset
-let AKK = 1, MIX = 2, DAT = 3, GEN = 4;
-let key = ["Nominative", "Accusative", "Two-way", "Dative", "Genitive"];
-let set = [[1, "bis"], [1, "entlang"], [1, "durch"], [1, "ohne"], [1, "gegen"], [1, "um"], [1, "für"], 
+let key = ["Accusative", "Dative", "Two-way", "Genitive"];
+let set = [[0, "bis"], [0, "entlang"], [0, "durch"], [0, "ohne"], [0, "gegen"], [0, "um"], [0, "für"], 
+            [1, "aus"], [1, "außer"], [1, "bei"], [1, "mit"], [1, "nach"], [1, "seit"], [1, "von"], [1, "zu"], 
             [2, "an"], [2, "auf"], [2, "in"], [2, "vor"], [2, "hinter"], [2, "über"], [2, "unter"], [2, "neben"], [2, "zwischen"],
-            [3, "aus"], [3, "außer"], [3, "bei"], [3, "mit"], [3, "nach"], [3, "seit"], [3, "von"], [3, "zu"], 
-            [4, "statt"], [4, "außerhalb"], [4, "innerhalb"], [4, "trotz"], [4, "während"], [4, "wegen"]]; // 30
+            [3, "statt"], [3, "außerhalb"], [3, "innerhalb"], [3, "trotz"], [3, "während"], [3, "wegen"]]; // 20
 
 let gameType = "preps";
 let blank = "ist _____";
