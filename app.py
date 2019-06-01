477912a63c124bbd5fb82b7f40e00d7844d03ea9
Derek Watson <derek.watson92@gmail.com>
2019-06-01 15:49:57 +0200

    reconfigured app, added login and register routes and an apology function to display application errors to user

--- app.py
+++ app.py
@@ -1,13 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import json
+import csv
 from game_data import *
 
 app = Flask(__name__)
-app.config['TESTING'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+# Temporary users home until database is integrated
+users = {"derek": "123", "anni": "password1", "tyler": "glasboi"}
+
+def apology(message, code=400):
+    return render_template("apology.html", code=code, message=message), code
 
 @app.route("/")
 def newGame():
   return render_template("new-game.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    session.clear()
+    if not request.form.get("username"):
+      return apology("must provide username", 403)
+    elif not request.form.get("password"):
+      return apology("must provide password", 403)
+
+    if request.form.get("username") in users:
+      if request.form.get("password") == users[f'{request.form.get("username")}']:
+        return apology("successful login", 200)
+      else:
+        return apology("incorrect password")
+    else:
+      return apology("username does not exsist")
+
+  else:
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if request.method == "POST":
+    session.clear()
+    if not request.form.get("username"):
+      return apology("must provide username", 403)
+    elif not request.form.get("password"):
+      return apology("must provide password", 403)
+    elif request.form.get("password") != request.form.get("repeat"):
+      return apology("passwords must match", 403)
+
+    users[f"{request.form.get('username')}"] = request.form.get("password")
+    return apology("registered!!", 200)
+
+  else:
+    return render_template("register.html")
 
 @app.route("/about")
 def about():
