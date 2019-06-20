06a2bc5d709f760609f328d3b6670fcd3473dd29
Derek Watson <derek.watson92@gmail.com>
2019-06-20 14:25:13 +0200

    minor organizational changes

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -32,13 +32,16 @@ function displayTable() {
     setTableSort();
 };
 
+
 function setTableSort() {
-    const getCellValue = (tr, index) => tr.children[index].innerText || tr.children[index].textContent;
+    const getCellValue = (tr, index) => {
+        return tr.children[index].innerText || tr.children[index].textContent;
+    };
 
     const comparer = (idx, asc) => (rowa, rowb) => ((txta, txtb) => {
             if (txta.slice(-1) == "%" && txtb.slice(-1) == "%") txta = txta.slice(0, -1), txtb = txtb.slice(0, -1);
             return txta !== '' && txtb !== '' && !isNaN(txta) && !isNaN(txtb) ? txta - txtb : txta.toString().localeCompare(txtb);
-        })(getCellValue(asc ? rowa : rowb, idx), getCellValue(asc ? rowb : rowa, idx));
+        })(getCellValue(asc ? rowa : rowb, idx), getCellValue(asc ? rowb : rowa, idx)); 
 
     const arrow = (order) => order ? "&#x1f809;" : "&#x1f80b;";
         
