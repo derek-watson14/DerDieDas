e5aee34041ced12df53b5e31a9cbb26ffa338672
Derek Watson <derek.watson92@gmail.com>
2019-06-20 14:28:05 +0200

    had to change results page to accomodate game information being saved while user still has tab open

--- static/scripts/results.js
+++ static/scripts/results.js
@@ -1,10 +1,24 @@
 let results = JSON.parse(localStorage.getItem("resultRecord"));
 let score = JSON.parse(localStorage.getItem("score"));
 let gameType = JSON.parse(localStorage.getItem("gameType"));
+localStorage.removeItem('savedGame')
+
+function displayResults(display) {
+    if (localStorage.getItem("resultRecord")) {
+      let msg = score[1] > 0 ? `You got ${score[0]} of ${score[1]} correct, or ${score[2]}%. ${setMessage(score[2])}` : "No exercises completed.";
+      document.querySelectorAll('.result-score')[0].innerHTML = msg;
+      document.querySelectorAll('.result-table')[0].innerHTML = buildTable(results, display);
+      setTableSort();
+    } else {
+      document.querySelectorAll('.result-score')[0].innerHTML = "No results to display.";
+    }
+};
 
 function buildTable(results, display) {
-  let tableHTML = `<tr class='row-heading'><th>Your Answer</th><th>Correct Answer</th>\
-                   <th>Word</th>${userRecord ? '<th>History</th>' : ''}</tr>`;
+  let tableHTML = `<tr class='row-heading'><th>Your Answer-</th>
+                                           <th>Correct Answer-</th>\
+                                           <th>Word-</th>
+                                           ${userRecord ? '<th>History-</th>' : ''}</tr>`;
   for (let item of results) {
     tableHTML += createRow(item, display);
   };
@@ -12,12 +26,18 @@ function buildTable(results, display) {
 };
 
 function createRow(item, display) {
-  let history = userRecord ? `<td>${getQuestionPercentage(item[0])}</td>` : "";
+  let history = userRecord ? `<td>${getQuestionPercentage(item.qID)}</td>` : "";
   if (display) {
-    let rowDeco = item[4] ? "class='row-correct'" : "class='row-incorrect'";
-    return `<tr ${rowDeco}><td>${item[1]}</td><td>${item[2]}</td><td>${item[3]}</td>${history}</tr>`;
+    let rowDeco = item.correctness ? "class='row-correct'" : "class='row-incorrect'";
+    return `<tr ${rowDeco}><td>${item.guess}</td>
+                           <td>${item.solution}</td>
+                           <td>${item.word}</td>
+                           ${history}</tr>`;
   } else {
-    return !item[4] ? `<tr class='row-incorrect'><td>${item[1]}</td><td>${item[2]}</td><td>${item[3]}</td>${history}</tr>` : "";
+    return !item.correctness ? `<tr class='row-incorrect'><td>${item.guess}</td>
+                                                          <td>${item.solution}</td>
+                                                          <td>${item.word}</td>
+                                                          ${history}</tr>` : "";
   }
 };
 
@@ -34,12 +54,26 @@ function setMessage(score) {
   else if (score < 25) return "Wow bitch, you dumb.";
 };
 
-function displayResults(display) {
-  if (localStorage) {
-    let msg = score[1] > 0 ? `You got ${score[0]} of ${score[1]} correct, or ${score[2]}%. ${setMessage(score[2])}` : "No exercises completed.";
-    document.querySelectorAll('.result-score')[0].innerHTML = msg;
-    document.querySelectorAll('.result-table')[0].innerHTML = buildTable(results, display);
-  } else {
-    document.querySelectorAll('.result-score')[0].innerHTML = "No results to display.";
-  }
+function setTableSort() {
+    const getCellValue = (tr, index) => {
+        return tr.children[index].innerText || tr.children[index].textContent;
+    };
+
+    const comparer = (idx, asc) => (rowa, rowb) => ((txta, txtb) => {
+            if (txta.slice(-1) == "%" && txtb.slice(-1) == "%") txta = txta.slice(0, -1), txtb = txtb.slice(0, -1);
+            return txta !== '' && txtb !== '' && !isNaN(txta) && !isNaN(txtb) ? txta - txtb : txta.toString().localeCompare(txtb);
+        })(getCellValue(asc ? rowa : rowb, idx), getCellValue(asc ? rowb : rowa, idx)); 
+
+    const arrow = (order) => order ? "&#x1f809;" : "&#x1f80b;";
+        
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+        const table = th.closest('table');
+        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+            .forEach(tr => table.appendChild(tr) );
+        Array.from(th.parentNode.children).forEach(head => {
+            const headtext = head.innerHTML.replace(/([\w ]+).+/, "$1");
+            head.innerHTML = `${headtext}${head == th ? arrow(this.order = !this.order) : "-"}`;
+        });
+    })));
 };
