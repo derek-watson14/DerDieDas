515da41442ef1f70ba0ca18be44e974932fc5aa9
Derek Watson <derek.watson92@gmail.com>
2019-06-28 09:21:07 +0200

    changed run game to handle setSize and maxScore from new-game.html form, added two helper functions to help accomplish this

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, flash, render_template, request, session, redirect
 from queries import (register_user, search_users, get_questions,
                      serialize_record, serialize_grades, update_gradebook,
-                     serialize_question, grades_for, profile_table)
+                     serialize_question, grades_for, profile_table,
+                     filter_qs)
 from werkzeug.security import check_password_hash
 from json import dumps
 from config import SECRET_KEY
@@ -65,9 +66,10 @@ def register():
         user = register_user(request.form.get("username"),
                              request.form.get("password"))
         if user:
-            session["user_id"] = user[0]
-            session["username"] = user[1]
-            flash(f"{user[1]} registered and logged in!")
+            session["user_id"] = user["u_id"]
+            session["username"] = user["username"]
+            session["reg_date"] = user["reg_date"].strftime("%B %dth, %Y")
+            flash(f"{user.username} registered and logged in!")
             return redirect("/")
         else:
             flash("Username taken")
@@ -98,38 +100,63 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/game/<gametype>")
+@app.route("/game/<gametype>", methods=["POST", "GET"])
 def run_game(gametype):
-    if gametype == "gender":
-        template = "gender.html"
-        questions = [serialize_question(question) for question
-                     in get_questions("gender")]
-    if gametype == "preps":
-        template = "prepositions.html"
-        questions = [serialize_question(question) for question
-                     in get_questions("preps")]
-    if gametype == "endings":
-        template = "endings.html"
-        questions = [serialize_question(question) for question
-                     in get_questions("endings")]
-    if gametype == "articles":
-        template = "articles.html"
-        questions = [serialize_question(question) for question
-                     in get_questions("articles")]
+    template = determine_template(gametype)
+    serialized = None
 
-    session["current_gametype"] = gametype
+    if request.method == "POST":
+        session["current_gametype"] = gametype
+
+        max_score = (normalize_max_score(int(request.form.get("maxScore")))
+                     if request.form.get("maxScore") else None)
+
+        questions = (filter_qs(gametype, max_score, session["user_id"])
+                     if max_score else get_questions(gametype))
+
+        serialized = [serialize_question(question) for question in questions]
+
+        full = len(serialized)
+        usr_len = (int(request.form.get("setSize"))
+                   if request.form.get("setSize") else None)
+
+        set_size = usr_len if usr_len and usr_len <= full else full
+        session["set_size"] = set_size
+
     return render_template(template,
                            gametype=gametype,
-                           questions=questions,
-                           user=dumps(get_u_id()))
+                           questions=serialized,
+                           setSize=session["set_size"],
+                           user=get_u_id())
+
+
+def determine_template(gametype):
+    if gametype == "gender":
+        return "games/gender.html"
+    if gametype == "preps":
+        return "games/prepositions.html"
+    if gametype == "endings":
+        return "games/endings.html"
+    if gametype == "articles":
+        return "games/articles.html"
+
+
+def normalize_max_score(max_score):
+    if max_score > 100:
+        return 100
+    elif max_score < 0:
+        return 0
+    else:
+        return max_score
 
 
 @app.route("/results")
 def results():
     session["current_gametype"] = None
+    session["set_size"] = None
     session["results"] = True
     user_record = None
-    if session:
+    if session.get("user_id"):
         user_record = [serialize_record(record) for record
                        in grades_for(session["user_id"])]
 
