b3a6d3b7fd14fc3708ad7fe0fce2b19200c931c2
Derek Watson <derek.watson92@gmail.com>
2019-06-28 08:46:18 +0200

    refactored from loose functions into three classes

--- static/scripts/results.js
+++ static/scripts/results.js
@@ -1,79 +1,93 @@
-let results = JSON.parse(localStorage.getItem("resultRecord"));
-let score = JSON.parse(localStorage.getItem("score"));
-let gameType = JSON.parse(localStorage.getItem("gameType"));
-localStorage.removeItem('savedGame')
-
-function displayResults(display) {
-    if (localStorage.getItem("resultRecord")) {
-      let msg = score[1] > 0 ? `You got ${score[0]} of ${score[1]} correct, or ${score[2]}%. ${setMessage(score[2])}` : "No exercises completed.";
-      document.querySelectorAll('.result-score')[0].innerHTML = msg;
-      document.querySelectorAll('.result-table')[0].innerHTML = buildTable(results, display);
-      setTableSort();
-    } else {
-      document.querySelectorAll('.result-score')[0].innerHTML = "No results to display.";
+class ResultDisplay {
+    constructor(tableController, messageController) {
+        this.displayAll = true;
+        this.toggle = document.querySelector('.form-btn');
+        this.tableController = tableController;
+        this.messageController = messageController;
     }
-};
-
-function buildTable(results, display) {
-  let tableHTML = `<tr class='row-heading'><th>Your Answer-</th>
-                                           <th>Correct Answer-</th>\
-                                           <th>Word-</th>
-                                           ${userRecord ? '<th>History-</th>' : ''}</tr>`;
-  for (let item of results) {
-    tableHTML += createRow(item, display);
-  };
-  return tableHTML;
-};
-
-function createRow(item, display) {
-  let history = userRecord ? `<td>${getQuestionPercentage(item.qID)}</td>` : "";
-  if (display) {
-    let rowDeco = item.correctness ? "class='row-correct'" : "class='row-incorrect'";
-    return `<tr ${rowDeco}><td>${item.guess}</td>
-                           <td>${item.solution}</td>
-                           <td>${item.word}</td>
-                           ${history}</tr>`;
-  } else {
-    return !item.correctness ? `<tr class='row-incorrect'><td>${item.guess}</td>
-                                                          <td>${item.solution}</td>
-                                                          <td>${item.word}</td>
-                                                          ${history}</tr>` : "";
-  }
-};
-
-function getQuestionPercentage(rq_id) {
-  record = userRecord.filter(item => item["rq_id"] == rq_id)[0];
-  return Math.round((record["correct"] / record["attempts"]) * 100).toString() + "%"; 
-};
-
-function setMessage(score) {
-  if (score > 90) return "Excellent score!";
-  else if (score > 75 && score <= 90) return "Well done!";
-  else if (score > 50 && score <= 75) return "Room for improvement.";
-  else if (score > 25 && score <= 50) return "Keep working at it.";
-  else if (score < 25) return "Wow bitch, you dumb.";
-};
-
-function setTableSort() {
-    const getCellValue = (tr, index) => {
-        return tr.children[index].innerText || tr.children[index].textContent;
-    };
-
-    const comparer = (idx, asc) => (rowa, rowb) => ((txta, txtb) => {
-            if (txta.slice(-1) == "%" && txtb.slice(-1) == "%") txta = txta.slice(0, -1), txtb = txtb.slice(0, -1);
-            return txta !== '' && txtb !== '' && !isNaN(txta) && !isNaN(txtb) ? txta - txtb : txta.toString().localeCompare(txtb);
-        })(getCellValue(asc ? rowa : rowb, idx), getCellValue(asc ? rowb : rowa, idx)); 
-
-    const arrow = (order) => order ? "&#x1f809;" : "&#x1f80b;";
-        
-    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-        const table = th.closest('table');
-        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-            .forEach(tr => table.appendChild(tr) );
-        Array.from(th.parentNode.children).forEach(head => {
-            const headtext = head.innerHTML.replace(/([\w ]+).+/, "$1");
-            head.innerHTML = `${headtext}${head == th ? arrow(this.order = !this.order) : "-"}`;
+    setInitialDisplay() {
+        localStorage.removeItem('savedGame');        
+        this.messageController.displayMessage();
+        this.tableController.displayTable(this.displayAll);
+        this.addButtonListener();
+        setTableSort();
+    }
+    addButtonListener() {
+        this.toggle.addEventListener('click', () => {
+            this.displayAll = !this.displayAll;
+            this.tableController.displayTable(this.displayAll);
+            if (this.displayAll) this.toggle.innerHTML = "Incorrect Only";
+            else this.toggle.innerHTML = "Show All";
+            setTableSort();
         });
-    })));
+    }
+}
+
+
+class TableController {
+    constructor(userRecord) {
+        this.userRecord = userRecord
+        this.results = JSON.parse(localStorage.getItem("resultRecord"));
+        this.gameType = JSON.parse(localStorage.getItem("gameType"));
+        this.table = document.querySelector('.result-table');
+    }
+    displayTable(display) {
+        localStorage.getItem("resultRecord") ? this.table.innerHTML = this.buildTable(display) : this.table = '';
+    }
+    buildTable(display) {
+        let tableHTML = `<tr class='row-heading'><th>Your Answer-</th>\
+                         <th>Correct Answer-</th>\
+                         <th>Word-</th>\
+                         ${this.userRecord ? '<th>History-</th>' : ''}</tr>`;
+        for (let item of this.results) {
+          tableHTML += this.createRow(item, display);
+        };
+        return tableHTML;
+    }
+    createRow(item, display) {
+        let history = userRecord ? `<td>${this.getUserScore(item.qID)}%</td>` : "";
+        if (display) {
+          let rowDeco = item.correctness ? "class='row-correct'" : "class='row-incorrect'";
+          return `<tr ${rowDeco}><td>${item.guess}</td>
+                                 <td>${item.solution}</td>
+                                 <td>${item.word}</td>
+                                 ${history}</tr>`;
+        } else {
+          return !item.correctness ? `<tr class='row-incorrect'><td>${item.guess}</td>
+                                      <td>${item.solution}</td>
+                                      <td>${item.word}</td>
+                                      ${history}</tr>` : "</tr>";
+        }
+    }
+    getUserScore(rq_id) {
+        return userRecord.find(item => item["rq_id"] === rq_id).score; 
+    };
 };
+
+
+class MessageController {
+    constructor() {
+        this.score = JSON.parse(localStorage.getItem("score"));
+        this.scoreElement = document.querySelector('.result-score');
+    }
+    displayMessage() {
+        if (localStorage.getItem("resultRecord")) {
+            let msg = this.score[1] > 0 ? `You got ${this.score[0]} of ${this.score[1]} correct, 
+                                           or ${this.score[2]}%. 
+                                           ${this.setMessage()}`
+                                        : "No exercises completed.";
+            this.scoreElement.innerHTML = msg;
+        } else {
+            this.scoreElement.innerHTML = "No results to display.";
+        }
+
+    }
+    setMessage() {
+        if ( this.score[2] > 90) return "Excellent score!";
+        else if (this.score[2] > 75 && this.score[2] <= 90) return "Well done!";
+        else if (this.score[2] > 50 && this.score[2] <= 75) return "Room for improvement.";
+        else if (this.score[2] > 25 && this.score[2] <= 50) return "Keep working at it.";
+        else if (this.score[2] < 25) return "Wow bitch, you dumb.";
+    };
+};
+
