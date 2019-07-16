32e4e1c27cc12691a420d5d2a5c35b1db98a5fd2
Derek Watson <derek.watson92@gmail.com>
2019-07-15 19:42:50 -0700

    configuration data

---
+++ config.py
@@ -0,0 +1,10 @@
+﻿# Scheme: 'postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>'
+
+# DATABASE_URI = ('postgres+psycopg2://postgres:BrodyPST!!7'
+#                 '@localhost:5432/DerDieDas')
+
+DATABASE_URI = ('postgres://zuhaedrtmlfbxs:d52094c4b4f7e2bb25e550d9ae4df5ef70'
+                '8c1d6eadb0c2e62052355df81530a4@ec2-174-129-226-232.compute-1'
+                '.amazonaws.com:5432/de7jtg2am2vs3j')
+
+SECRET_KEY = '2\xf3\xd6\xbf\x85\x08@\x18\x9cG\x8a\x95\x06c\xb9('
